984da2b7748424aef156cec770f20a2af793fda3
olga_demina <olga.demina1410@gmail.com>
2022-05-18 10:32:53 +0200

    Correzione banner icone

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppPrefooter />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppPrefooter from "./components/AppPrefooter.vue" 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppPrefooter
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./style/common.scss";
+
 </style>
---
+++ src/components/AppPrefooter.vue
@@ -0,0 +1,72 @@
+<template>
+  <section class="prefooter">
+    <div class="container">
+      <ul class="icons-list">
+        <li v-for="(item,index) in icons" :key="index">
+          <img :src="require(`../assets/img/${item.img}`)" alt="" />
+          <h3>{{ item.text }}</h3>
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "AppPrefooter",
+  data: function() {
+    return {
+      icons: [
+        {
+          img: 'buy-comics-digital-comics.png',
+          text: 'Digital Comics'
+        },
+        {
+          img: 'buy-comics-merchandise.png',
+          text: 'DC merchandise'
+        },
+        {
+          img: 'buy-comics-subscriptions.png',
+          text: 'Subscription'
+        },
+        {
+          img: 'buy-comics-shop-locator.png',
+          text: 'Comic shop locator'
+        },
+        {
+          img: 'buy-dc-power-visa.svg',
+          text: 'DC power visa'
+        }
+      ]
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.prefooter {
+  background-color: blue;
+  color: white;
+  padding: 2rem 0;
+
+  .icons-list {
+    display: flex;
+
+    li {
+      width: calc(100% / 5);
+      display: flex;
+      align-items: center;
+      padding: .5rem;
+
+      img {
+        width: 20%;
+        margin-right: 1rem;
+      }
+
+      h3 {
+
+      }
+    }
+  }
+}
+</style>
